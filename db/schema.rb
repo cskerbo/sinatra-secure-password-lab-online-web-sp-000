f26cdd46bc52de06e584c95d276ef7d58640a4ac
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-13 03:46:53 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20191113034317) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
